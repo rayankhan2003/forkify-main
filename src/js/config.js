3fbc7802c0b9b20329f501910728cbc9b392d53e
rayankhan2003 <rayan4khan1@gmail.com>
2024-05-21 22:55:43 +0500

    forkify

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '90289726-41a6-4233-ae14-eced6a8fc067';
+export const MODAL_CLOSE_SEC = 2.5;
